a8757b5f94cad907de42c6c5a369cdbfd5b02323
Alejandro <alejandro@AcerNotebook.bitdron.com>
2019-01-29 22:06:58 +0100

    Add  new routes

--- express/server.js
+++ express/server.js
@@ -1,15 +1,31 @@
 'use strict';
 const express = require('express');
-const serverless = require('serverless-http');
+// const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
+
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
+
+router.get('/foo', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.write('<h1>FOOOOOO!</h1>');
+  res.end();
+});
+
+router.get('/bar', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.write('<h1>BAR!</h1>');
+  res.end();
+});
+
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
@@ -17,4 +33,4 @@ app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 module.exports = app;
-module.exports.handler = serverless(app);
+// module.exports.handler = serverless(app);
